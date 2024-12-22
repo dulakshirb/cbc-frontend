fd04e690c576ccd3ea2856514336f35532c35fea
Dulakshi RB <dulakshirb@gmail.com>
2024-12-22 17:45:24 +0530

    Add react hot toast 📦

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homePage";
 import AdminHomePage from "./pages/admin/adminHomePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Toaster
+          position="top-right"
+        />
         <Routes path="/*">
           <Route path="/*" element={<HomePage />} />
-          <Route path="/admin/*" element={<AdminHomePage/>} />
+          <Route path="/admin/*" element={<AdminHomePage />} />
         </Routes>
       </BrowserRouter>
     </>
