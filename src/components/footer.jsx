bf0b2ebdddf1304d05a7db8ff9206f4c84c0fda1
Dulakshi RB <dulakshirb@gmail.com>
2024-12-22 16:24:49 +0530

    Improve header and footer design 🎨

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div className="bg-black text-white flex justify-center items-center p-4 h-10">
+        <div className="bg-secondary text-text flex justify-center items-center p-4 h-10">
             <p>&copy; 2024 CBC Inc.</p>
         </div>
     );
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div className="bg-black flex justify-between items-center p-4 h-[90px]">
-      <div className="text-4xl text-yellow-600 font-bold font-mono">CBC</div>
-      <div className="flex items-center gap-5 text-white">
+    <div className="bg-secondary flex justify-between items-center p-4 h-[90px]">
+      <img className="w-10" src="/logo.webp" alt="" />
+      <div className="flex items-center gap-4 text-text">
         <Link to="/" >Home</Link>
         <Link to='/products'>Products</Link>
         <Link to="/contact">Contact</Link>
         <Link to='/login'>Login</Link>
-        <Link className="bg-white text-yellow-600 p-2 rounded-lg" to='/signup'>Get Started</Link>
+        <Link className="bg-primary text-white uppercase text-sm font-semibold p-2 rounded-lg" to='/signup'>Get Started</Link>
       </div>
     </div>
   );
