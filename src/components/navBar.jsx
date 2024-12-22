b4f3fe8b2fd89d4ae649b01b20cab23dda911449
Dulakshi RB <dulakshirb@gmail.com>
2024-12-22 16:42:54 +0530

    Adjust the logo 🎨

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div className="bg-secondary flex justify-between items-center p-4 h-[90px]">
-      <img className="w-10" src="/logo.webp" alt="" />
+      <img className="sm:w-[60px] w-[45px]" src="/logo.webp" alt="" />
       <div className="flex items-center gap-4 text-text">
         <Link to="/" >Home</Link>
         <Link to='/products'>Products</Link>
